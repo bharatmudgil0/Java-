18a9175f8dcb782a67a8772f22eb4dc6869916c1
bharatmudgil0 <mudgilbharat0@gmail.com>
2025-11-17 20:53:52 +0530

    Question 2 in external Pratical.

--- Final_Pratical/Question_2.java
+++ Final_Pratical/Question_2.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.util.Scanner;
 
-public class EmployeeJDBC {
+public class Question_2 {
 
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/emp";
