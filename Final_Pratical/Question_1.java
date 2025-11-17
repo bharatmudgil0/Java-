729028b746a555c5da7f3cd4c17e6104026f44e4
bharatmudgil0 <mudgilbharat0@gmail.com>
2025-11-17 20:53:14 +0530

    Question 1 is external Pratical.

--- Final_Pratical/Question_1.java
+++ Final_Pratical/Question_1.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Main {
+public class Question_1 {
     public static void main(String[] args) {
 
         ArrayList<String> list = new ArrayList<>();
