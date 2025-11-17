046f0e3cfc493ab15b89d0337ae7c523a5a8977c
bharatmudgil0 <mudgilbharat0@gmail.com>
2025-11-17 20:52:11 +0530

    Question 1 is external Pratical

---
+++ Final_Pratical/Question_1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Mango");
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Grapes");
+
+        Collections.sort(list);
+
+        System.out.println(list);
+    }
+}
